f99f79ed3f56b4768dcfd197e8085d7f0d315533
scriptcrux <scriptcrux.tech@gmail.com>
2024-03-01 22:14:25 +0530

    init repo

---
+++ tests/test.spec.ts
@@ -0,0 +1,25 @@
+//@ts-check
+import test, { expect } from '@playwright/test';
+
+test.describe('navigation',()=>{
+
+    test.beforeEach(async ({ page }) => {
+       await page.goto('https://playwright.dev/'); 
+    });
+
+    test('has title', async ({ page }) => {
+    //   await page.goto('https://playwright.dev/');
+
+      await expect(page).toHaveTitle(/Playwright/);
+    });
+
+    test('get started link', async ({ page }) => {
+    //   await page.goto('https://playwright.dev/');
+
+      await page.getByRole('link', { name: 'Get Started' }).click();
+
+      await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    });
+    
+})
+
